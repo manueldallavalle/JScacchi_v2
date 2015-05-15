c37ed6a09e9c8d4ba4c9517db47d237766482756
davideleone <davideleone94@gmail.com>
2015-05-15 10:02:13 +0200

    Ottimizzato Torre con gli ArrayList

--- JScacchi/src/Torre.java
+++ JScacchi/src/Torre.java
@@ -1,4 +1,5 @@
 import java.awt.Point;
+import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
 
@@ -10,26 +11,25 @@ public class Torre extends Pezzo{
 	public Torre(Colore colore){
 		super((colore.equals(Colore.BIANCO)) ? new ImageIcon("immagini/torre_bianca.gif") : new ImageIcon("immagini/torre_nera.gif"),colore );
 	}
-	
+	@Override
 	public Pezzi getPezzo(){
 		return Pezzi.TORRE;
 	}
 	
 	//getX()=colonne
 	//getY()=righe
-	public Point[] getMovimento(){
+	public ArrayList<Point> getMovimento(){
 		int i=0,check_bordo_dx=1,check_bordo_sx=0,check_bordo_down=1,check_bordo_up=0;
-		Point punti[];
+		ArrayList<Point> punti = new ArrayList<>();
 		check_bordo_sx=getX();
-		punti=new Point[14];
 		while(i<7){    //getX()+check_bordo_dx)<=7 && check_bordo_sx>=0
 			if(getX()+check_bordo_dx<=7){
-				punti[i].setLocation(getY(),(getX()+(check_bordo_dx)));
+				punti.add(new Point(getY(),(getX()+(check_bordo_dx))));
 				i++;
 				check_bordo_dx++;
 			}
 			if((check_bordo_sx-1)>=0){
-				punti[i].setLocation(getY(),check_bordo_sx-1);
+				punti.add(new Point(getY(),check_bordo_sx-1));
 				i++;
 				check_bordo_sx--;
 			}
@@ -37,12 +37,12 @@ public class Torre extends Pezzo{
 		check_bordo_up=getY();
 		while(i<14){    //getX()+check_bordo_dx)<=7 && check_bordo_sx>=0
 			if(getY()+check_bordo_down<=7){
-				punti[i].setLocation(getY()+check_bordo_down,getX());
+				punti.add(new Point(getY()+check_bordo_down,getX()));
 				i++;
 				check_bordo_down++;
 			}
 			if((check_bordo_up-1)>=0){
-				punti[i].setLocation(check_bordo_up-1,getX());
+				punti.add(new Point(check_bordo_up-1,getX()));
 				i++;
 				check_bordo_up--;
 			}
