fc442427aa7483450d6a422227ee4d6e6bdfbe40
Manuel Dalla Valle <manuel.dallavalle0@gmail.com>
2015-05-15 11:25:19 +0200

    Utilizzo variabili esterne ; Gestione turni
    * Ora utilizzo variabili esterne salvate sulla scacchiera principale (non più final static) con appoggio tramite setter - getter
    * Implementata la gestione dei turni con relativi errori

--- JScacchi/src/MonitorAzioni.java
+++ JScacchi/src/MonitorAzioni.java
@@ -6,31 +6,36 @@ import struttura.*;
 
 public class MonitorAzioni implements ActionListener {
 	private StrutturaScacchiera scacchiera;
-	private static Info stato;
-	
+
 	public MonitorAzioni(StrutturaScacchiera scacchiera){
 		this.scacchiera = scacchiera;
-		this.stato = Info.TURNO_BIANCHI;
 	}
 	
 	public void actionPerformed(ActionEvent e) {
 		Pezzo p_click = (Pezzo) e.getSource();
+		Info stato = scacchiera.getStato();
 
-		// 1° CLICK
 		if(stato.equals(Info.TURNO_BIANCHI) || stato.equals(Info.TURNO_NERI)){
 			if(p_click.getPezzo().equals(Pezzi.VUOTO)){
 				JOptionPane.showMessageDialog(null, Messaggi.ERR_PEZZONULL.getMsg(), "Errore!", JOptionPane.ERROR_MESSAGE);
+			}else if((p_click.getColore().equals(Colore.BIANCO) && stato.equals(Info.TURNO_NERI)) ||
+					(p_click.getColore().equals(Colore.NERO) && stato.equals(Info.TURNO_BIANCHI))){
+				String turno_avversari = ((stato.equals(Info.TURNO_BIANCHI)) ? Messaggi.MSG_TRBIANCHI.getMsg() : Messaggi.MSG_TRNERI.getMsg());
+				JOptionPane.showMessageDialog(null, turno_avversari, "Info", JOptionPane.WARNING_MESSAGE);
 			}else{
-				evidenziaCaselle();
+				JOptionPane.showMessageDialog(null, p_click.getLocation().getX() + " - " + p_click.getLocation().getY());
+				evidenziaCaselle(p_click);
 				// AGGIORNAMENTO STATO
-				stato = ((stato.equals(Info.TURNO_BIANCHI)) ? Info.ATTESA_BIANCHI : Info.ATTESA_NERI);
+				scacchiera.setPzAttesa(p_click);
+				scacchiera.setStato((stato.equals(Info.TURNO_BIANCHI)) ? Info.ATTESA_BIANCHI : Info.ATTESA_NERI);
 			}
 		}else if(stato.equals(Info.ATTESA_BIANCHI) || stato.equals(Info.ATTESA_NERI)){
 			
+			scacchiera.incrementaMosse(); // Incrementa contatore
 		}
 	}
 	
-    protected int checkSpostamento(Pezzo old, Pezzo nw) {
+    protected int checkSpostamento(Pezzo newPezzo) {
         /**
          * STESSO COLORE -- MOSSA NON VALIDA
          * COLORE DIVERSO -- MOSSA VALIDA
@@ -38,19 +43,20 @@ public class MonitorAzioni implements ActionListener {
          * 0 = MANGIATA VUOTA
          * 1 = PEZZO MANGIATO
          */
+    	Pezzo old = scacchiera.getPzAttesa();
         
-        if ((old.getColore()).equals(nw.getColore())) {
+        if ((old.getColore()).equals(newPezzo.getColore())) {
             return -1;
-        } else if (nw.getColore().equals(Colore.VUOTO)) {
+        } else if (newPezzo.getColore().equals(Colore.VUOTO)) {
             return 0;
-        }else if (!(old.getColore()).equals(nw.getColore())){
+        }else if (!(old.getColore()).equals(newPezzo.getColore())){
         	return 1;
         }
         
         return -1;
     }
 	
-	public void evidenziaCaselle(){
-		
+	public void evidenziaCaselle(Pezzo p_click){
+		Pezzo[][] t = scacchiera.getTavolo();
 	}
 }
