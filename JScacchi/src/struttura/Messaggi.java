508737b512a21a5a4cb663a7bbdb20760030c1ae
Manuel Dalla Valle <manuel.dallavalle0@gmail.com>
2015-05-15 11:26:46 +0200

    Aggiunta messaggi gestione turni
    * Aggiunti messaggi turno bianchi/neri

--- JScacchi/src/struttura/Messaggi.java
+++ JScacchi/src/struttura/Messaggi.java
@@ -1,7 +1,9 @@
 package struttura;
 
 public enum Messaggi {
-	ERR_PEZZONULL ("Seleziona una pedina valida!");
+	ERR_PEZZONULL ("Seleziona una pedina valida!"),
+	MSG_TRBIANCHI ("E' il turno dei bianchi!"),
+	MSG_TRNERI ("E' il turno dei neri");
 	
 
 	private Messaggi(String msg){
