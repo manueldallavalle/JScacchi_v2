1a7bf89111705a23e314326fb5e64fcb7c225d7d
davideleone <davideleone94@gmail.com>
2015-05-15 11:25:42 +0200

    Update Cavallo.java

--- JScacchi/src/Cavallo.java
+++ JScacchi/src/Cavallo.java
@@ -6,38 +6,29 @@ import javax.swing.ImageIcon;
 import struttura.Colore;
 import struttura.Pezzi;
 
-public class Pedone extends Pezzo{
+public class Cavallo extends Pezzo{
 	private static final long serialVersionUID = 1L;
-
-	public Pedone(Colore colore){
-		super((colore.equals(Colore.BIANCO)) ? new ImageIcon("immagini/pedone_bianca.gif") : new ImageIcon("immagini/pedone_nera.gif"),colore );
+	public Cavallo(Colore colore){
+		super((colore.equals(Colore.BIANCO)) ? new ImageIcon("immagini/cavallo_bianca.gif") : new ImageIcon("immagini/cavallo_nera.gif"),colore );
 	}
 	@Override
 	public Pezzi getPezzo(){
-		return Pezzi.PEDONE;
+		return Pezzi.CAVALLO;
 	}
 	
 	@Override
 	public ArrayList<Point> getMovimento(){
+		
+		final int[] posix_riga={2,-2,1,-1};
+		final int[] posix_colonna={1,-1,2,-2};
 		ArrayList<Point> punti = new ArrayList<>();
-		if(getColore().equals(Colore.NERO)){
-			punti.add(new Point((int)(getLocation().getY()+1),(int)(getLocation().getX())));
-				if(getX()+1<=7){
-					punti.add(new Point((int)(getLocation().getY()+1),(int)(getLocation().getX()+1)));
+			for(int i=0;i<4;i++){
+				for(int j=0;j<2;j++){
+						if(getY()+posix_riga[i]<8 && getY()+posix_riga[i]>-1 && getX()+posix_colonna[j]<8 && getX()+posix_colonna[j]>-1){
+							punti.add(new Point((int)(getLocation().getY()+posix_riga[i]),(int)(getLocation().getX()+posix_colonna[j])));
+						}
 				}
-				if(getX()-1>=7){
-					punti.add(new Point((int)(getLocation().getY()+1),(int)(getLocation().getX()-1)));
-				}
-		}
-		else{
-			punti.add(new Point((int)(getLocation().getY()-1),(int)(getLocation().getX())));
-			if(getX()+1<=7){
-				punti.add(new Point((int)(getLocation().getY()-1),(int)(getLocation().getX()+1)));
 			}
-			if(getX()-1>=7){
-				punti.add(new Point((int)(getLocation().getY()-1),(int)(getLocation().getX()-1)));
-			}
-		}
 		return punti;
 	}
 }
