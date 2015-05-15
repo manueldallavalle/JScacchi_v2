abd03f88fb5ed6c32a8eee1d625772009631fc3f
davideleone <davideleone94@gmail.com>
2015-05-15 09:59:44 +0200

    Ottimizzato Re con gli ArrayList

--- JScacchi/src/Re.java
+++ JScacchi/src/Re.java
@@ -1,4 +1,5 @@
 import java.awt.Point;
+import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
 
@@ -11,24 +12,20 @@ public class Re extends Pezzo{
 	public Re(Colore colore){
 		super((colore.equals(Colore.BIANCO)) ? new ImageIcon("immagini/re_bianca.gif") : new ImageIcon("immagini/re_nera.gif"),colore );
 	}
-	
+	@Override
 	public Pezzi getPezzo(){
 		return Pezzi.RE;
 	}
 	
 	//getX()=colonne
 	//getY()=righe
-	public Point[] getMovimento(){
-		Point[] punti;
-		punti=new Point[8];
+	public ArrayList<Point> getMovimento(){
+		ArrayList<Point> punti = new ArrayList<>();
 		final int[] posix_riga={1,1,0,-1,-1,-1,0,1};
 		final int[] posix_colonna={0,1,1,1,0,-1,-1,-1};
 		for(int i=0;i<8;i++){
 			if((getY()+posix_riga[i]<=7 && getY()+posix_riga[i]>=0) && getX()+posix_colonna[i]<=7 && getX()+posix_colonna[i]>=0){
-				punti[i].setLocation(getY()+posix_riga[i],getX()+posix_colonna[i]);
-			}
-			else{
-				punti[i].setLocation(null);
+				punti.add(new Point(getY()+posix_riga[i],getX()+posix_colonna[i]));
 			}
 		}
 		return punti;
