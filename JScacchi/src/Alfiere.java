0070326523e9290ff91b082983c6f08eca6f37ba
davideleone <davideleone94@gmail.com>
2015-05-15 09:54:20 +0200

    Ottimizzato Alfiere con gli ArrayList

--- JScacchi/src/Alfiere.java
+++ JScacchi/src/Alfiere.java
@@ -1,4 +1,5 @@
 import java.awt.Point;
+import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
 
@@ -11,35 +12,26 @@ public class Alfiere extends Pezzo{
 	public Alfiere(Colore colore){
 		super((colore.equals(Colore.BIANCO)) ? new ImageIcon("immagini/alfiere_bianca.gif") : new ImageIcon("immagini/alfiere_nera.gif"),colore );
 	}
-	
+	@Override
 	public Pezzi getPezzo(){
 		return Pezzi.ALFIERE;
 	}
 	
 	//getX()=colonne
 	//getY()=righe
-	public Point[] getMovimento(){
-		Point[] punti;
-		punti=new Point[13];
-		int index=0;
+	public ArrayList<Point> getMovimento(){
+		ArrayList<Point> punti = new ArrayList<>();
 		for(int i=getY(),j=getX();(i<=7 && j<=7);i++,j++){
-			punti[index].setLocation(i+1,j+1);
-			index++;
+			punti.add(new Point(i+1,j+1));
 		}
 		for(int i=getY(),j=getX();(i>=0 && j<=7);i--,j++){
-			punti[index].setLocation(i-1,j+1);
-			index++;
+			punti.add(new Point(i-1,j+1));
 		}
 		for(int i=getY(),j=getX();(i>=0 && j>=0);i--,j--){
-			punti[index].setLocation(i-1,j-1);
-			index++;
+			punti.add(new Point(i-1,j-1));
 		}
 		for(int i=getY(),j=getX();(i<=7 && j>=0);i++,j--){
-			punti[index].setLocation(i+1,j-1);
-			index++;
-		}
-		for(;index<12;index++){
-			punti[index].setLocation(null);
+			punti.add(new Point(i+1,j-1));
 		}
 		return punti;
 	}
