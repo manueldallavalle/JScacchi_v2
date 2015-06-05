9ff68092ed3e2eabe7204e76d4b7f0667003a756
Manuel Dalla Valle <manuel.dallavalle0@gmail.com>
2015-06-05 12:13:29 +0200

    * Aggiornamento messaggi errore
    * Aggiunto ERR_NOMOSSE
    * Aggiunto ERR_MOSSAVALIDA

--- JScacchi/src/struttura/Messaggi.java
+++ JScacchi/src/struttura/Messaggi.java
@@ -2,8 +2,11 @@ package struttura;
 
 public enum Messaggi {
 	ERR_PEZZONULL ("Seleziona una pedina valida!"),
+	ERR_NOMOSSE ("Non ci sono mosse disponibili!"),
+	ERR_MOSSAVALIDA ("Movimento non ammesso!"),
 	MSG_TRBIANCHI ("E' il turno dei bianchi!"),
 	MSG_TRNERI ("E' il turno dei neri");
+	
 	
 
 	private Messaggi(String msg){
